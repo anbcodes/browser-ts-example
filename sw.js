0989a6d3c6e03b095d9f18804b1852961f0f274b
anbcodes <anbcodes@protonmail.com>
2022-01-09 23:05:12 -0500

    removed scope from package name

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
-importScripts("https://unpkg.com/@anbcodes/browser-ts@0.0.4/sw.js");
+// To avoid issues with chrome, the version must be specficed in order to avoid a redirect by unpkg
+importScripts("https://unpkg.com/browser-ts@0.0.5/sw.js");
